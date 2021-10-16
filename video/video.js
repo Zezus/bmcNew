fec93f3a5ae3a0aa5e84eb089900b9559879034f
Zezus <rustam-sadvakaso@mail.ru>
2021-10-17 04:29:48 +0600

    Update video.js

--- video/video.js
+++ video/video.js
@@ -1,7 +1,13 @@
-var splide = new Splide( '.p3_flex1_phone', {
-    video: {
-        autoplay: true,
-        mute    : true,
+var swiper = new Swiper(".p3_flex1_phone", {
+    effect: "cube",
+    grabCursor: true,
+    cubeEffect: {
+        shadow: true,
+        slideShadows: true,
+        shadowOffset: 20,
+        shadowScale: 0.94,
+    },
+    pagination: {
+        el: ".swiper-pagination",
     },
 });
-splide.mount();
